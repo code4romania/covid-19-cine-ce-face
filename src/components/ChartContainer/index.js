161d926dad9938a1562303f2681326572f1f9a46
Teodora Dima <teo.dima@gmail.com>
2020-03-28 13:48:19 +0200

    #4 Add chart container with dummy data source

---
+++ src/components/ChartContainer/index.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import "./styles.scss";
+
+const CHART_URL = "https://public.flourish.studio/resources/embed.js";
+const CHART_DATA_SOURCE = "visualisation/1710354"; // TODO replace with real data source
+
+const ChartContainer = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.src = CHART_URL;
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div
+      className="chart-container flourish-embed flourish-network"
+      data-src={CHART_DATA_SOURCE}
+      data-height="400px"
+    />
+  );
+};
+
+export default ChartContainer;
