ce29c5d9d064f6a082e405fc27969c9289ab36d6
Tudor Amariei <tudor.amariei@figshare.com>
2020-05-13 14:43:51 +0300

    Update the footer
    - remove old two-column links
    - add links according to the UX specs

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import {
   Footer,
   FooterLinkHeader,
-  FooterLinks,
-  FooterLinkItem
+  FooterLinkItem,
+  FooterLinks
 } from "@code4ro/taskforce-fe-components";
 
 const FooterWrapper = () => {
@@ -12,75 +13,36 @@ const FooterWrapper = () => {
       <FooterLinks>
         <FooterLinkHeader>Link-uri utile</FooterLinkHeader>
         <FooterLinkItem>
-          <a
-            href="https://www.gov.ro/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Guvernul României
-          </a>
-        </FooterLinkItem>
-        <FooterLinkItem>
-          <a
-            href="http://www.dsu.mai.gov.ro/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Departamentul pentru <br />
-            Situații de Urgență
-          </a>
-        </FooterLinkItem>
-        <FooterLinkItem>
-          <a href="http://www.ms.ro/" target="_blank" rel="noopener noreferrer">
-            Ministerul Sănătății
-          </a>
-        </FooterLinkItem>
-      </FooterLinks>
-      <FooterLinks>
-        <FooterLinkHeader>&nbsp;</FooterLinkHeader>
-        <FooterLinkItem>
-          <a
-            href="https://stirioficiale.ro/informatii"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Știri Oficiale
-          </a>
-        </FooterLinkItem>
-        <FooterLinkItem>
-          <a
-            href="https://cetrebuiesafac.ro/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Ce trebuie să fac
-          </a>
-        </FooterLinkItem>
-        <FooterLinkItem>
-          <a
-            href="https://datelazi.ro/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Date la zi
-          </a>
-        </FooterLinkItem>
-        <FooterLinkItem>
-          <a
-            href="https://rohelp.ro/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            RoHelp
-          </a>
-        </FooterLinkItem>
-        <FooterLinkItem>
-          <a
-            href="http://www.code4.ro/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href="https://code4.ro/" target="_blank" rel="noopener noreferrer">
             Code for Romania
+          </a>
+        </FooterLinkItem>
+        <FooterLinkItem>
+          <a
+            href="https://github.com/code4romania/whos-who/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Codul sursă în GitHub
+          </a>
+        </FooterLinkItem>
+        <FooterLinkItem>
+          <Link to="/termeni-si-conditii-de-utilizare">
+            Termeni și condiții de utilizare
+          </Link>
+        </FooterLinkItem>
+        <FooterLinkItem>
+          <Link to="/politica-de-confidentialitate">
+            Politică de confidențialitate
+          </Link>
+        </FooterLinkItem>
+        <FooterLinkItem>
+          <a
+            href="mailto:contact@code4.ro"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Contact
           </a>
         </FooterLinkItem>
       </FooterLinks>
