61495dbf31f2f4aac088904fb223aebd622a6c3e
Tudor Amariei <tudor.amariei@figshare.com>
2020-05-13 17:32:26 +0300

    fix ESLint

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -66,7 +66,19 @@ const Home = () => {
         <Hero
           title={"Bine ai venit"}
           useFallbackIcon={true}
-          subtitle={<React.Fragment>”Cine ce face” este un proiect de informare prin care sperăm să ajutăm cetățenii să aibă o imagine cât mai clară și corectă asupra rolurilor diverșilor actori din societate în situația de pandemie cu care ne confruntăm. Proiectul este unul în continuă dezvoltare și vom completa informațiile în curând cu actorii internaționali și organizațiile neguvernamentale. Dacă observi orice neconcordanță, că lipsește un actor relevant, sau că informația este neclară, te rugăm să ne <a href="mailto:contact@code4.ro">transmiți un e-mail</a>.</React.Fragment>}  
+          subtitle={
+            <React.Fragment>
+              ”Cine ce face” este un proiect de informare prin care sperăm să
+              ajutăm cetățenii să aibă o imagine cât mai clară și corectă asupra
+              rolurilor diverșilor actori din societate în situația de pandemie
+              cu care ne confruntăm. Proiectul este unul în continuă dezvoltare
+              și vom completa informațiile în curând cu actorii internaționali
+              și organizațiile neguvernamentale. Dacă observi orice
+              neconcordanță, că lipsește un actor relevant, sau că informația
+              este neclară, te rugăm să ne{" "}
+              <a href="mailto:contact@code4.ro">transmiți un e-mail</a>.
+            </React.Fragment>
+          }
         />
       </div>
 
