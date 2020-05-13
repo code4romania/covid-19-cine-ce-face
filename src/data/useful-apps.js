9e7aaa7bc7cd03ed894ddc1fb864044c952196f0
Alex Stefanescu <s.alexandra.stefanescu@gmail.com>
2020-05-13 18:16:27 +0300

    Add new useful instruments

--- src/data/useful-apps.js
+++ src/data/useful-apps.js
@@ -1,64 +1,59 @@
 export default [
   {
-    title: "Recomandări oficiale",
+    title: "Instalează-ți extensia de Chrome pentru a depista știrile false",
     doc_type: "USEFUL_APP",
     doc_id: 1,
     active: true,
     display_order: 1,
-    app_type: "INFO",
-    content: "15 RECOMANDĂRI privind conduita socială responsabilă în prevenirea răspândirii coronavirus",
+    app_type: "NEWS",
     buttons: [
       {
-        title: "Citește aici",
-        link: "https://stirioficiale.ro/15-recomandari-privind-conduita-sociala-responsabila-in-prevenirea-raspandirii-coronavirus"
+        title: "Instalează extensie",
+        link:
+          "https://chrome.google.com/webstore/detail/covid-19-stiri-oficiale/pdcpkplohipjhdfdchpmgekifmcdbnha"
       }
     ]
-  }
-  ,
+  },
   {
-    title: "Instalează-ți extensia de Chrome pentru a depista știrile false",
+    title: "Instalează-ți extensia de Firefox pentru a depista știrile false",
     doc_type: "USEFUL_APP",
     doc_id: 2,
     active: true,
     display_order: 2,
     app_type: "NEWS",
-    content: null,
     buttons: [
       {
         title: "Instalează extensie",
-        link: "https://chrome.google.com/webstore/detail/covid-19-stiri-oficiale/pdcpkplohipjhdfdchpmgekifmcdbnha"
+        link:
+          "https://addons.mozilla.org/en-US/firefox/addon/covid-19-%C8%99tiri-oficiale/"
       }
     ]
-  }
-  ,
+  },
   {
-    title: "Instalează-ți extensia de Firefox pentru a depista știrile false",
+    title: "Știri oficiale și informații la zi",
     doc_type: "USEFUL_APP",
     doc_id: 3,
     active: true,
     display_order: 3,
     app_type: "NEWS",
-    content: null,
-    buttons: [
-      {
-        title: "Instalează extensie",
-        link: "https://addons.mozilla.org/en-US/firefox/addon/covid-19-%C8%99tiri-oficiale/"
-      }
-    ]
-  }
-  ,
-  {
-    title: "Știri oficiale și informații la zi",
-    doc_type: "USEFUL_APP",
-    doc_id: 4,
-    active: true,
-    display_order: 4,
-    app_type: "NEWS",
-    content: null,
     buttons: [
       {
         title: "Cele mai noi informații oficiale",
         link: "https://stirioficiale.ro/"
+      }
+    ]
+  },
+  {
+    title: "Ce trebuie să fac?",
+    doc_type: "USEFUL_APP",
+    doc_id: 4,
+    active: true,
+    display_order: 4,
+    app_type: "INFO",
+    buttons: [
+      {
+        title: "Toate informațiile de care ai nevoie",
+        link: "https://cetrebuiesafac.ro"
       }
     ]
   },
@@ -69,12 +64,26 @@ export default [
     active: true,
     display_order: 5,
     app_type: "DATA",
-    content: null,
     buttons: [
       {
         title: "Vezi aici date la zi",
         link: "https://datelazi.ro/"
       }
     ]
-  }
+  },
+  {
+    title: "Vrei să ajuți. Intră aici",
+    doc_type: "USEFUL_APP",
+    doc_id: 6,
+    active: true,
+    display_order: 6,
+    app_type: "OFFER_HELP",
+    content: null,
+    buttons: [
+      {
+        title: "Donează prin RoHelp",
+        link: "https://rohelp.ro/"
+      }
+    ]
+  },
 ];
